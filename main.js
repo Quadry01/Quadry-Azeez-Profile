7e0a6f6cf3ddc704e4b97758f40b78a4b854296c
Quadry01 <quadryazeez01@gmail.com>
2022-12-31 13:20:40 +0100

    Mobile view

--- main.js
+++ main.js
@@ -7,3 +7,10 @@ document.querySelector(".close-btn").addEventListener("click", function () {
   document.querySelector(".overlay-box").classList.remove("overlay");
   document.querySelector(".project-div").classList.add("display");
 });
+
+document
+  .querySelector(".project-link-mini")
+  .addEventListener("click", function () {
+    document.querySelector(".overlay-box").classList.add("overlay");
+    document.querySelector(".project-div").classList.remove("display");
+  });
