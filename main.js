25cead488b12032f769795253fed7fe997304145
Quadry01 <quadryazeez01@gmail.com>
2023-01-06 00:43:29 +0100

    website refactored

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 document.querySelector(".project-link").addEventListener("click", function () {
   document.querySelector(".overlay-box").classList.add("overlay");
   document.querySelector(".project-div").classList.remove("display");
+  document.querySelector(".close-logic").classList.remove("close-logic");
 });
 
-document.querySelector(".close-btn").addEventListener("click", function () {
-  document.querySelector(".overlay-box").classList.remove("overlay");
-  document.querySelector(".project-div").classList.add("display");
-});
+document
+  .querySelector(".fa-circle-xmark")
+  .addEventListener("click", function () {
+    document.querySelector(".overlay-box").classList.remove("overlay");
+    document.querySelector(".project-div").classList.add("display");
+    document.querySelector(".close-logic").classList.add("close-logic");
+  });
 
 document
   .querySelector(".project-link-mini")
